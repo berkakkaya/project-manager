891267bbf56f6d28961a4382e99a27f9a9ff73a9
Berk Akkaya <berkakkaya000@gmail.com>
2020-04-19 16:25:41 +0300

    Fix some docstrings in input_controller.py
    Some docstrings were incorrect in terms of grammar. They are fixed.

--- src/input_controller.py
+++ src/input_controller.py
@@ -8,17 +8,17 @@ class InputController(object):
     Methods
     -------
     get_bool(prompt : str) -> bool
-        Prompts user for yes/no question and checks for answer is yes(y) or no(n).
+        Checks if the answer is yes(y) or no(n).
     
     get_path(prompt : str) -> str
-        Prompts user for path and checks for path is exists.
+        Checks if the path that is specified in answer is exists.
     
     get_str(prompt : str) -> str
-        Prompts user for any text and checks for answer is blank.
+        Checks if the answer is blank.
         Can be used if answer is required.
     
     get_option(prompt : str, choices:list) -> int
-        Prompts user for option and checks for selection is in choices
+        Checks if answer is a selectable option.
     """
 
     def __init__(self):
@@ -26,7 +26,7 @@ class InputController(object):
 
     def get_path(self, prompt: str) -> str:
         """
-        Prompts user for path and checks for given path is exists
+        Checks if the path that is specified in answer is exists.
 
         Parameters
         ----------
@@ -47,7 +47,7 @@ class InputController(object):
     
     def get_bool(self, prompt: str) -> bool:
         """
-        Prompts user for yes(y) or no(n) and checks for answer is yes(y) or no(n)
+        Checks if the answer is yes(y) or no(n).
 
         Parameters
         ----------
@@ -72,8 +72,8 @@ class InputController(object):
     
     def get_str(self, prompt: str) -> str:
         """
-        Prompts user for any text and checks for answer is blank
-        Can be used if answer is required
+        Checks if the answer is blank.
+        Can be used if answer is required.
 
         Parameters
         ----------
@@ -94,7 +94,7 @@ class InputController(object):
     
     def get_option(self, prompt: str, choices: list) -> int:
         """
-        Prompts user for option and checks for selection is in choices
+        Checks if answer is a selectable option.
 
         Parameters
         ----------
